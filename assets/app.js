0e8336ba74e7524b04c42fa17e19b35e763fab21
ArmelBouvier <60919177+ArmelBouvier@users.noreply.github.com>
2020-11-17 10:59:43 +0100

    creation of the navbar

--- assets/app.js
+++ assets/app.js
@@ -6,8 +6,8 @@
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.css';
-import 'bootstrap/dist/bootstrap.min.css';
+//import './styles/app.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
 // import $ from 'jquery';
