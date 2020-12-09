d797ebbf04d8e2cb25901d0f418f5bf3842df935
ArmelBouvier <60919177+ArmelBouvier@users.noreply.github.com>
2020-12-09 11:42:31 +0100

    Admin part of the Academic presentation accomplished

--- src/Controller/Admin/AcademicController.php
+++ src/Controller/Admin/AcademicController.php
@@ -18,10 +18,10 @@ class AcademicController extends AbstractController
     /**
      * @Route("/", name="academic_browse", methods={"GET"})
      */
-    public function browse(): Response
+    public function browse(AcademicRepository $academicRepository): Response
     {
-        return $this->render('movie/browse.html.twig', [
-            'movies' => $movieRepository->findAll(),
+        return $this->render('admin/academic/browse.html.twig', [
+            'formations' => $academicRepository->findAll(),
         ]);
     }
 
@@ -41,13 +41,54 @@ class AcademicController extends AbstractController
             $entityManager->persist($academic);
             $entityManager->flush();
 
-            $this->addFlash('success', 'Formation '. $academic->getDiplomaTitle() . ' ajoutée');
+            $this->addFlash('success', 'Formation ' . $academic->getDiplomaTitle() . ' ajoutée');
 
             return $this->redirectToRoute('academic_browse');
         }
 
         return $this->render('admin/academic/add.html.twig', [
             'academic' => $academic,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/delete/{id}", name="academic_delete", requirements={"id":"\d+"}, methods={"DELETE"})
+     */
+    public function delete(Academic $academic): Response
+    {
+        $em = $this->getDoctrine()->getManager();
+        $em->remove($academic);
+        $em->flush();
+
+        $this->addFlash('success', 'Formation supprimée');
+        return $this->redirectToRoute('academic_browse');
+    }
+
+    /**
+     * @Route("/edit/{id}", name="academic_edit", requirements={"id":"\d+"})
+     */
+    public function edit(Academic $academic, Request $request)
+    {
+        $form = $this->createForm(AcademicType::class, $academic);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Le formulaire est valide, on change la valeur du $updatedAt de $academic
+            $academic->setUpdatedAt(new \DateTime());
+
+            // On peut flusher les modifications
+            // Inutile de persister car l'entity manager connait déjà cet objet
+            // Inutile de récupérer le manager dans $em juste pour écrire $em->flush() ensuite
+            // On peut donc tout faire en une seule ligne
+            $this->getDoctrine()->getManager()->flush();
+
+            // On peut rediriger l'utilisateur vers la liste des formations
+            $this->addFlash('success', 'Formation modifiée');
+            return $this->redirectToRoute('academic_browse');
+        }
+
+        return $this->render('admin/academic/edit.html.twig', [
             'form' => $form->createView(),
         ]);
     }
